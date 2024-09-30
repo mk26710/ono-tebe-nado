57daa7bc8c44a0d729f1907731c6ea632f0d3464
mk26710 <type-iv@mk26710.dev>
2024-09-30 19:43:05 +0400

    feat: disable caching on dev server

--- server.go
+++ server.go
@@ -5,9 +5,19 @@ import (
 	"net/http"
 )
 
+func NoCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("./"))
-	http.Handle("/", fs)
+	http.Handle("/", NoCache(fs))
 
 	log.Println("Running on :3000")
 	err := http.ListenAndServe(":3000", nil)
